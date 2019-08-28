91a3b7ffa646dd69ad5d51945cebbb1cceae2d88
shaycohen988 <shaycohen988@gmail.com>
2019-08-28 23:25:54 +0300

    a
    a

---
+++ src/RecipesApp.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+export default function RecipesApp() {
+  const APP_KEY = "7544ed837eb08e30fcae4872309b7963";
+  const APP_ID = "630e94e4";
+
+  const req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(req);
+      console.log(result);
+    }
+    fetchData();
+  });
+
+  return (
+    <div>
+      <h1> RecipesשדדApp</h1>
+    </div>
+  );
+}
---
+++ src/seedRecipes.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    name: "Fruit Salad",
+    discription:
+      "The perfect fruit salad for a backyard bbq or any occasion. There are never leftovers! This is one of my favorite fruit salad recipes, as I think the sauce really makes it. This salad is tastier the longer you can let it soak in its juices. I prefer 3 to 4 hours in the refrigerator before I serve it. Enjoy.",
+      ingredients : [
+          "2/3 cup of fresh orange juice",
+          "3 kiwi fruit, peeled and sliced",
+          "boil water",
+      ]
+  },
+  {
+    id: 2
+  }
+];
